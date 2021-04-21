f59a5a573cfea93c23d7c09fa69a133273950548
One <lucja.adamska.pl@gmail.com>
2021-04-22 01:19:40 +0300

    Forms&dates

--- index.php
+++ index.php
@@ -17,9 +17,34 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 }
 
 echo "</table>";
+echo "<br>";
+
 ?>
 
-<form method="GET" action = "index.php">
-    <input type="date" name="date" />
-    <input type="submit" value="Показать" name="submit" />
-</form>
+    <form method="POST" action="index.php">
+        <input type="number" name="date" min="1000" max="9999"/>
+        <input type="submit" value="Показать" name="submit"/>
+    </form>
+
+<?php
+
+if (isset($_POST['submit'])) {
+    $number = ($_POST['date']);
+
+    echo "<br>";
+    $sql = "SELECT * FROM `users` WHERE `bdate` LIKE'$number%'";
+    $result = $conn->query($sql);
+
+    echo "<table width='300px'><tr><th>id</th><th>first_name</th><th>last_name</th><th>bdate</th></tr>";
+
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        echo "<tr align='center'>";
+        echo "<td>" . $row['id'] . "</td>";
+        echo "<td>" . $row['first_name'] . "</td>";
+        echo "<td>" . $row['last_name'] . "</td>";
+        echo "<td>" . $row['bdate'] . "</td>";
+        echo "</tr>";
+    }
+    echo "<table>";
+}
+?>
