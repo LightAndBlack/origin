f0595e9905fa65b1801c272f2102f7f5fb5296cd
One <lucja.adamska.pl@gmail.com>
2021-04-22 14:08:28 +0300

    working on pdo prepare (placeholders)

--- index.php
+++ index.php
@@ -24,16 +24,22 @@ echo "<br>";
     <form method="POST" action="index.php">
         <input type="number" name="date" min="1000" max="9999"/>
         <input type="submit" value="Показать" name="submit"/>
+        <input type="text" name="text"> <!-- Проверка для экранирования -->
     </form>
 
 <?php
 
 if (isset($_POST['submit'])) {
     $number = ($_POST['date']);
-
+    $text = ($_POST['text']); echo $text;// <script>alert("hi");</script> для проверки на экранирование, а как это связать с PDO prepare???
     echo "<br>";
+
     $sql = "SELECT * FROM `users` WHERE `bdate` LIKE'$number%'";
     $result = $conn->query($sql);
+
+    //https://tproger.ru/translations/how-to-configure-and-use-pdo/
+    //Подготовленные запрос
+//    $date = $conn->prepare("INSERT INTO users(bdate) VALUES(?, ?)");
 
     echo "<table width='300px'><tr><th>id</th><th>first_name</th><th>last_name</th><th>bdate</th></tr>";
 
