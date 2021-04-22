11dacba46ed358a028f6a8228767c616d897159a
One <lucja.adamska.pl@gmail.com>
2021-04-22 22:03:36 +0300

    is it working pdo prepare (placeholders) ?

--- index.php
+++ index.php
@@ -34,8 +34,16 @@ if (isset($_POST['submit'])) {
     $text = ($_POST['text']); echo $text;// <script>alert("hi");</script> для проверки на экранирование, а как это связать с PDO prepare???
     echo "<br>";
 
-    $sql = "SELECT * FROM `users` WHERE `bdate` LIKE'$number%'";
-    $result = $conn->query($sql);
+//    $sql = "SELECT * FROM `users` WHERE `bdate` LIKE'$number%'";
+    $sql = $conn -> prepare("SELECT * FROM `users` WHERE `bdate` LIKE'$number%'");
+    $sql -> execute();
+    //    $sql = $conn ->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+//        $sql = $conn ->prepare("SELECT * FROM `users` WHERE `bdate` LIKE'$number%'");
+//        $sql -> execute();
+//    $sql ->execute(array('3'));
+//    $array = $sql->fetch(PDO::FETCH_ASSOC);
+//    $result =$conn->query($sql);
+//    $sql =$conn->query($sql);
 
     //https://tproger.ru/translations/how-to-configure-and-use-pdo/
     //Подготовленные запрос
@@ -43,7 +51,7 @@ if (isset($_POST['submit'])) {
 
     echo "<table width='300px'><tr><th>id</th><th>first_name</th><th>last_name</th><th>bdate</th></tr>";
 
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    while ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
         echo "<tr align='center'>";
         echo "<td>" . $row['id'] . "</td>";
         echo "<td>" . $row['first_name'] . "</td>";
